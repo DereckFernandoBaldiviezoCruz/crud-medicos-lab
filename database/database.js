c5a19ce9310d821377c15576c6568ef7d6201c6b
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-12 18:53:19 -0400

    Version 1.1

--- database/database.js
+++ database/database.js
@@ -1,15 +1,26 @@
+// database.js
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+export const sequelize = new Sequelize({
   dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+// Verificar conexión
+async function testDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+// Llamar a la función para probar la conexión
+testDatabaseConnection();
+
+export default sequelize;
