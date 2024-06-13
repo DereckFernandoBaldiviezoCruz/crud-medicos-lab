5c146159bf73d121df6741a3e428832ff19e516e
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-13 13:37:44 -0400

    Version 1

--- models/medic.js
+++ models/medic.js
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
-import { sequelize } from '../database/database.js';
+import { db } from '../database/database.js';
 
-export const Medic = sequelize.define('Medic', {
+export const Medic = db.define('Medic', { 
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
