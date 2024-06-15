8ae85171113abfc53768c1472c8c8232d7d02804
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-15 14:54:14 -0400

    Version 3

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,3 +1,5 @@
+// auth.controllers.js
+
 import User from '../models/user.js';
 
 // Login de usuario
@@ -12,8 +14,14 @@ export const login = async (req, res) => {
     // Guarda el usuario en la sesión
     req.session.user = user;
 
-    // Envía una respuesta de éxito indicando el rol del usuario
-    res.json({ role: user.role });
+    // Redirecciona según el rol del usuario
+    if (user.role === 'admin') {
+      res.redirect('/admin'); // Redirige al dashboard de admin
+    } else if (user.role === 'patient') {
+      res.redirect('/patient'); // Redirige al dashboard de paciente
+    } else if (user.role === 'medic') {
+      res.redirect('/medic'); // Redirige al dashboard de médico
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
