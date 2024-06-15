d525464f75c2cb1ea5ca17ddc1094113f8c4c14c
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-15 12:46:30 -0400

    Version 3

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
-import session from 'express-session';
 import db from './database/database.js';
 import userRoutes from './routes/user.routes.js';
-import authRoutes from './routes/auth.routes.js';
+import authRoutes from './routes/auth.routes.js';  // Importa las rutas de autenticación
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,29 +17,47 @@ app.set('views', './views');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'yourSecretKey',
-  resave: false,
-  saveUninitialized: true,
-}));
 
-app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/auth', authRoutes);  // Asigna el prefijo '/auth' a las rutas de autenticación
 
+// Ruta para la página de inicio
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index');  // Renderiza la vista 'index.pug'
 });
 
+// Ruta para la página de login
+app.get('/login', (req, res) => {
+  res.render('login');  // Renderiza la vista 'login.pug'
+});
+
+// Ruta para la búsqueda de usuarios
+app.get('/users/search', async (req, res) => {
+  const { query } = req.query;
+  try {
+    const users = await User.findAll({
+      where: {
+        [Sequelize.Op.or]: [
+          { fullname: { [Sequelize.Op.iLike]: `%${query}%` } },
+          { username: { [Sequelize.Op.iLike]: `%${query}%` } },
+          { role: { [Sequelize.Op.iLike]: `%${query}%` } }
+        ]
+      }
+    });
+    res.render('index_users', { users });  // Renderiza la vista 'index_users.pug' con los resultados de la búsqueda
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Conexión a la base de datos y arranque del servidor
 db.authenticate()
   .then(() => {
     console.log('Database connected');
-    return db.sync({ force: false });
-  })
-  .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((error) => {
-    console.error('No se pudo conectar a la base de datos: ', error);
+    console.error('Unable to connect to the database:', error);
   });
