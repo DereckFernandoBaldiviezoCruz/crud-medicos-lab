efd6fc001d5defb6de3744a82fd4c065cbbffdc7
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-15 15:01:37 -0400

    Version 3

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -6,12 +6,15 @@ import {
   getAppointment,
   updateAppointment,
   deleteAppointment
-} from '../controllers/cita.controllers.js';
+} from '../controllers/appointment.controllers.js';
 
 const router = Router();
 
 router.post('/', createAppointment);
 router.get('/', getAppointments);
+router.get('/new', (req, res) => {
+  res.render('appointment_form'); // Renderiza el formulario de creación de cita
+});
 router.get('/:id', getAppointment);
 router.put('/:id', updateAppointment);
 router.delete('/:id', deleteAppointment);
