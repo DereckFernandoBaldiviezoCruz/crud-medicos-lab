67c4f05b721274b509f7ea52dc652bfec57dfe9c
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-16 12:47:47 -0400

    Verson 3

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -16,36 +16,24 @@ const router = Router();
 router.get('/', getAllAppointments);
 
 router.get('/new', async (req, res) => {
-  const { user } = req.session; // Extrae el usuario de la sesión
-  if (!user) {
-    return res.redirect('/login'); // Redirige al login si no hay usuario en sesión
-  }
-
   try {
-    // Carga los datos necesarios según el rol del usuario
-    let medics = [];
-    let patients = [];
+    // Cargar todos los médicos y pacientes
+    const medics = await Medic.findAll({
+      include: [{ model: User, attributes: ['fullname'] }],
+    });
+    const patients = await Patient.findAll({
+      include: [{ model: User, attributes: ['fullname'] }],
+    });
 
-    if (user.role === 'patient') {
-      // Si el usuario es un paciente, carga los médicos
-      medics = await Medic.findAll({
-        include: [{ model: User, attributes: ['fullname'] }],
-      });
-    } else if (user.role === 'medic') {
-      // Si el usuario es un médico, carga los pacientes
-      patients = await Patient.findAll({
-        include: [{ model: User, attributes: ['fullname'] }],
-      });
-    }
-
-    // Renderiza el formulario de citas con los datos del usuario en sesión y los datos cargados
-    res.render('appointment_form', { user, medics, patients });
+    // Renderizar el formulario de citas con los datos de médicos y pacientes
+    res.render('appointment_form', { medics, patients });
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
 
 router.get('/:id', getAppointmentById);
+router.post('/', createAppointment); // Asegúrate de que la ruta POST está definida correctamente
 router.post('/:id', updateAppointment);
 router.post('/:id/delete', deleteAppointment);
 
